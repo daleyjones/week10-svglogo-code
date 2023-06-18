f71f6d37323a06b20791132c68224b505e577f94
daleyjones <djones_15@hotmail.com>
2023-06-18 19:35:26 +0100

    shape commit

--- shapes.test.js
+++ shapes.test.js
@@ -8,27 +8,43 @@ describe("circle", () => {
     const actualSvg = circle.render();
     expect(actualSvg).toEqual(expectedSvg);
   });
+
+  test("should accept a fillColor param", () => {
+    const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="blue" />';
+    const circle = new Circle();
+    circle.setColor("blue");
+    const actualSvg = circle.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
 });
 
 describe("square", () => {
-  test("should render svg for blue square element", () => {
-    const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="blue" />';
+  test("should render svg for a green polygob element", () => {
+    const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="darkblue" />';
     const square = new Square();
-    square.setColor("blue");
+    square.setColor("darkblue");
+    const actualSvg = square.render();
+    expect(actualSvg).toEqual(expectedSvg);
+  });
+
+  test("should accept a fillColor param", () => {
+    const expectedSvg = '<rect x="90" y="40" width="120" height="120" fill="red" />';
+    const square = new Square();
+    square.setColor("red");
     const actualSvg = square.render();
     expect(actualSvg).toEqual(expectedSvg);
   });
 });
 
 describe("triangle", () => {
-  test("should render svg for orange triangle element", () => {
+  test("should render svg for green polygon element", () => {
     const expectedSvg = '<polygon points="150,18 244,182 56,182" fill="orange" />';
     const triangle = new Triangle();
     triangle.setColor("orange");
     const actualSvg = triangle.render();
     expect(actualSvg).toEqual(expectedSvg);
   });
-  
+
   test("should accept a fillColor param", () => {
     const expectedSvg = '<polygon points="150,18 244,182 56,182" fill="purple" />';
     const triangle = new Triangle();
