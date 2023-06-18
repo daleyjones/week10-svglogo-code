cbab495dee943a6c0feee3fe960d6709b537366f
daleyjones <djones_15@hotmail.com>
2023-06-18 18:56:21 +0100

    shape commit

---
+++ shapes.js
@@ -0,0 +1,45 @@
+class Shape {
+    constructor(color = '#000000') {
+      this.color = color;
+    }
+  
+    setColor(color) {
+      this.color = color;
+    }
+  }
+  
+  class Circle extends Shape {
+    constructor(color) {
+      super(color);
+    }
+  
+    render() {
+      const svg = `<circle cx="100" cy="100" r="80" fill="${this.color}" />`;
+      return svg;
+    }
+  }
+  
+  class Square extends Shape {
+    constructor(color) {
+      super(color);
+    }
+  
+    render() {
+      const svg = `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
+      return svg;
+    }
+  }
+  
+  class Triangle extends Shape {
+    constructor(color) {
+      super(color);
+    }
+  
+    render() {
+      const svg = `<polygon points="150,18 244,182 56,182" fill="${this.color}" />`;
+      return svg;
+    }
+  }
+  
+  module.exports = { Circle, Square, Triangle };
+  
