3175dc070ca31ccd619195dc74301f3ba26467dc
daleyjones <djones_15@hotmail.com>
2023-06-18 17:04:33 +0100

    cli commit

--- cli.js
+++ cli.js
@@ -1,71 +1,54 @@
 const inquirer = require('inquirer');
-const { join } = require('path');
 const { writeFile } = require('fs/promises');
-const { createDocument } = require('./document');
+const { Circle, Triangle, Square } = require('./shapes');
+const questions = require('./questions');
+const { LogoText, SVG } = require('./svg');
 
 class CLI {
-  constructor() {
-    this.title = '';
+  constructor() {}
 
-    // Array of task objects e.g. [{ text: string, priority: bool }, ...]
-    this.tasks = [];
-  }
   run() {
     return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'name',
-          message: 'Please enter your name',
-        },
-      ])
-      .then(({ name }) => {
-        this.title = `${name}'s Tasks`;
-        return this.addTask();
+      .prompt(questions)
+      .then((responses) => {
+        const svg = this.generateSVG(responses);
+
+        const markup = svg.render();
+
+        return writeFile('logo.svg', markup);
       })
-      .then(() => {
-        // sort by priority so that priority tasks come before non-priority tasks
-        this.tasks.sort((a, b) =>
-          a.priority === b.priority ? 0 : a.priority && !b.priority ? -1 : 1
-        );
-        return writeFile(
-          join(__dirname, '..', 'output', 'tasks.html'),
-          createDocument(this.title, this.tasks)
-        );
-      })
-      .then(() => console.log('Created tasks.html'))
+      .then(() => console.log('Logo successfully created!'))
       .catch((err) => {
         console.log(err);
         console.log('Oops. Something went wrong.');
       });
   }
 
-  addTask() {
-    return inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'text',
-          message: 'Enter task',
-        },
-        {
-          type: 'confirm',
-          name: 'priority',
-          message: 'Is this a priority task?',
-        },
-        {
-          type: 'confirm',
-          name: 'confirmAddTask',
-          message: 'Would you like to add another task?',
-        },
-      ])
-      .then(({ text, priority, confirmAddTask }) => {
-        this.tasks.push({ text, priority });
-        if (confirmAddTask) {
-          return this.addTask();
-        }
-      });
+  generateSVG(options) {
+    const logoText = new LogoText(options.text, options.textColour);
+
+    switch (options.shape) {
+      case 'circle':
+        const circle = new Circle(options.shapeColour);
+        return new SVG(circle, logoText);
+      case 'square':
+        const square = new Square(options.shapeColour);
+        return new SVG(square, logoText);
+      case 'triangle':
+        const triangle = new Triangle(options.shapeColour);
+        return new SVG(triangle, logoText);
+    }
   }
 }
+
+module.exports =CLI;
+
+
+
+
+
+
+}
+
 
 module.exports = CLI;
