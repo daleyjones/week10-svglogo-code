0e308493a9fd3c18f14af35c3ac54f867f987e91
daleyjones <djones_15@hotmail.com>
2023-06-18 20:51:11 +0100

    logo commit

--- library/Svg.js
+++ library/Svg.js
@@ -1,29 +1,28 @@
 class LogoText {
-    constructor(text, color) {
-      this.color = color;
-      this.text = text;
-    }
-    
-    render() {
-      return `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>`;
-    }
+  constructor(text, color) {
+    this.color = color;
+    this.text = text;
   }
-  
-  class SVG {
-    constructor(shape, logoText) {
-      this.shape = shape;
-      this.logoText = logoText;
-    }
-  
-    render() {
-      const svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${this.shape.render()}
-      ${this.logoText.render()}
-      </svg>`;
-  
-      return svg;
-    }
+
+  render() {
+    return `<text x="100" y="100" font-size="50" text-anchor="middle" fill="${this.color}">${this.text}</text>`;
   }
-  
-  module.exports = { LogoText, SVG };
-  
+}
+
+class SVG {
+  constructor(shape, logoText) {
+    this.shape = shape;
+    this.logoText = logoText;
+  }
+
+  render() {
+    const svg = `<svg version="1.1" width="800" height="400" xmlns="http://www.w3.org/2000/svg">
+    ${this.shape.render()}
+    ${this.logoText.render()}
+    </svg>`;
+
+    return svg;
+  }
+}
+
+module.exports = { LogoText, SVG };
